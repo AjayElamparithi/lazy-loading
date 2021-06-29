cf2fdd2316968a2d5316900d0f1fdae7be266a4c
Ajay E <ajayaj7094@gmail.com>
2021-06-29 14:53:18 +0530

    Second commit

--- src/app/admin/login/login.component.ts
+++ src/app/admin/login/login.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  title = "Learning Gives Creativity";
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  submit(){
+    console.log("Hi Welcome");
   }
 
 }
